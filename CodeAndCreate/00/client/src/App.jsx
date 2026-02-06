1fde76944d90fa07a6a8ce99605e524ec8bb0f3b
GersonESS <gebhsantos@gmail.com>
2026-02-06 19:28:28 -0300

    e

--- CodeAndCreate/00/client/src/App.jsx
+++ CodeAndCreate/00/client/src/App.jsx
@@ -3,18 +3,13 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div className='bg-red-500 container'>
-      <h1 className='text-5xl font-bold text-white'>
-        Tailwind CSS 4.0
-      </h1>
-      <button className='bg-amber text-alabaster  text-2x1 font-bold mt-12 border-4  px-6 py-4 rounded animate-pulse '>
-        Button
-      </button>
-      <input
-        type='text'
-        placeholder='Input Field'
-        className='bg-white text-lg text-amber-500  mt-6 p-4 border-4 rounded-md'
-      />
+    <div className='@container border p-4'>
+      <div className='grid gird-cols-1 @sm:grid-cols-2 @lg:grid-cols-4 gap-4'>
+        <div className='bg-gray-200 p-4'>Item 1</div>
+        <div className='bg-blue-200 p-4'>Item 2</div>
+        <div className='bg-green-200 p-4'>Item 3</div>
+        <div className='bg-red-200 p-4'>Item 4</div>
+      </div>
     </div>
   )
 }
