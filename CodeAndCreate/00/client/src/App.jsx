7658c15707322e934020e04dc2ba11a7018a1e88
GersonESS <gebhsantos@gmail.com>
2026-02-06 16:06:32 -0300

    a

--- CodeAndCreate/00/client/src/App.jsx
+++ CodeAndCreate/00/client/src/App.jsx
@@ -3,11 +3,11 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div className='bg-red-500 p-12'>
+    <div className='bg-red-500 container'>
       <h1 className='text-5xl font-bold text-white'>
         Tailwind CSS 4.0
       </h1>
-      <button className='bg-blue-500 text-white text-2x1 font-bold px-6 border-4  py-4 rounded-md mt-6'>
+      <button className='bg-amber text-alabaster  text-2x1 font-bold mt-12 border-4  px-6 py-4 rounded animate-pulse '>
         Button
       </button>
       <input
